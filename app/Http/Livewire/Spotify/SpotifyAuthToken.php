10ab496bb988b927b3b0bdef1cb11d2cfc8409b9
Shawn Weigand <shawnweigand1@gmail.com>
2022-01-05 22:51:39 -0500

    initial http

--- app/Http/Livewire/Spotify/SpotifyAuthToken.php
+++ app/Http/Livewire/Spotify/SpotifyAuthToken.php
@@ -2,12 +2,14 @@
 
 namespace App\Http\Livewire\Spotify;
 
+use Illuminate\Support\Facades\Http;
 use Livewire\Component;
 
 class SpotifyAuthToken extends Component
 {
 
     public $response = 'placeholder';
+    public $result;
 
     public function render()
     {
@@ -16,7 +18,13 @@ class SpotifyAuthToken extends Component
 
     public function authToken()
     {
-        $this->response = 'hello';
-        dd($this->response);
+        $result = Http::get('https://accounts.spotify.com/authorize?'.
+                            'client_id='.env('SPOTIFY_CLIENT_ID').'&'.
+                            'response_type=code&'.
+                            'redirect_uri='.urlencode('http://localhost:8000/').'&'.
+                            'scope=user-top-read user-read-recently-played');
+        //need to redirect
+        // $this->response = 'hello';
+        dd($result);
     }
 }
