fccf028f50484763791e6b26e45eb852b503f351
Shawn Weigand <shawnweigand1@gmail.com>
2022-02-08 00:40:14 -0500

    namespace work

--- app/Http/Livewire/Spotify/SpotifyAuthToken.php
+++ app/Http/Livewire/Spotify/SpotifyAuthToken.php
@@ -4,8 +4,9 @@ namespace App\Http\Livewire\Spotify;
 
 use Illuminate\Support\Facades\Http;
 use Livewire\Component;
+use SpotifyWebAPI\Session;
 
-require __DIR__.'/../../../../vendor/autoload.php';
+require __DIR__ . '/../../../../vendor/autoload.php';
 
 class SpotifyAuthToken extends Component
 {
@@ -19,15 +20,26 @@ class SpotifyAuthToken extends Component
 
     public function authToken()
     {
-        require __DIR__.'/../../../../vendor/autoload.php';
-
-        $session = new SpotifyWebAPI\Session(
-            'CLIENT_ID',
-            'CLIENT_SECRET',
-            'REDIRECT_URI'
+        $session = new Session(
+            env('SPOTIFY_CLIENT_ID'),
+            env('SPOTIFY_CLIENT_SECRET'),
+            env('SPOTIFY_REDIRECT_URI')
         );
-        dd(__DIR__.'/../../../../vendor/autoload.php');
-        $result = Http::get();
-        dd($result);
+
+        $state = $session->generateState();
+        $options = [
+            'scope' => [
+                'playlist-read-private',
+                'user-read-private',
+            ],
+            'state' => $state,
+        ];
+
+        header('Location: ' . $session->getAuthorizeUrl($options));
+        die();
+
+        // dd(__DIR__.'/../../../../vendor/autoload.php');
+        // $result = Http::get();
+        // dd($result);
     }
 }
